d0655ad2624b2d960021a304f5ea5075bb467f2a
locke <lockeCucumber@163.com>
2018-01-28 16:22:17 +0800

    完成初版工作

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 # coding:utf-8
 
 import tornado.web
-from tornado.options import define, options
+from tornado.options import define, options, parse_command_line
 from tornado.httpserver import HTTPServer
 from tornado.ioloop import IOLoop
 
@@ -21,7 +21,7 @@ def make_app():
     return tornado.web.Application(urls, **settings)
 
 if __name__ == '__main__':
-    tornado.options.parse_command_line()
+    parse_command_line()
 
     app = make_app()
     server = HTTPServer(app)
